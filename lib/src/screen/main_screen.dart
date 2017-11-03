df9d9f9b24ece0c95400d6f898fec6f12a565acd
Art Astillero <nanix84@yahoo.com>
2017-11-03 09:02:13 +0800

    mainscreen event changes

--- lib/src/screen/main_screen.dart
+++ lib/src/screen/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/MainFrameAuth.dart';
 import 'package:myapp/src/screen/main_drawer.dart';
-import 'package:myapp/src/dao/UserDao.dart';
+import 'package:myapp/src/widget/EventsTile.dart';
 
 class MainScreen extends StatefulWidget {
 
@@ -34,45 +34,171 @@ class _MainScreenState extends State<MainScreen> {
       MainFrameDrawer.currentUser=usr;
     });*/
   }
-  
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return new MaterialApp(
-      theme: new ThemeData(
-        //primaryColor: new Color(0xFF2c3b54),
+    //ThemeData theme = Theme.of(context);
+    ThemeData theme = new ThemeData(
+        primaryColor: new Color(0xFF324261),
+        fontFamily: "Montserrat-Regular",
         canvasColor: new Color(0xFF324261)
-      ),
+    );
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: theme,
       home: new Scaffold(
-          key: _scaffoldKey,
-          appBar: new AppBar(
-            title: new Text("Main Frame Dance Studio"),
-            automaticallyImplyLeading: false,
-            leading: new IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: _menuPressed
-            ),
+        key: _scaffoldKey,
+        drawer: new MainFrameDrawer(_scaffoldKey),
+        appBar: new AppBar(
+          title: new Text("Event"),
+          automaticallyImplyLeading: false,
+          leading: new IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: _menuPressed
           ),
-          body: new Container(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                new Padding(padding: new EdgeInsets.all(6.0)),
-                new Text("<<< MAIN SCREEN >>>"),
-                new Padding(padding: new EdgeInsets.all(6.0)),
-                new RaisedButton(
-                    child: new Text("Logout"),
-                    color: Colors.red,
-                    onPressed: () {
-                      _logout();
-                      //Navigator.of(context).pushNamedAndRemoveUntil("/", (_) => false);
-                    }
-                )
-              ],
-            ),
-          ),
+        ),
+        backgroundColor: const Color(0xFF324261),
+        body: new Container(
+          margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+          child: new Column(
+            children: <Widget>[
+              new Container(
+                decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                        image: new ExactAssetImage("mainframe_assets/images/m7x5ba.jpg"),
+                        fit: BoxFit.cover
+                    )
+                ),
+                child: new Container(
+                  height: 55.0,
+                  width: 200.0,
+                  child: new Text("Find Your Next Event Below",
+                    style: new TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0
+                    ),
+                  ),
+                ),
+                height: 235.0,
+                width: 360.0,
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
+              ),
 
-          drawer: new MainFrameDrawer(_scaffoldKey)
+              new Container(
+                //color: Colors.amber,
+                  height: 275.0,
+                  child: new ListView(
+                    children: <Widget>[
+                      new ClipRect(
+                          child: new Stack(
+                            children: <Widget>[
+                              new Container(
+                                  child: new Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                                      children: <Widget>[
+                                        // list items
+                                        new EventsListTile(
+                                          leading: new SizedBox(
+                                            height: 75.0,
+                                            width: 140.0,
+                                            child: new Image.network("http://i67.tinypic.com/63x7qe.png"),
+                                          ),
+                                          title: new Text("American Star Ball", style: new TextStyle(color: Colors.white)),
+                                          subtitle: new Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              new Text("May 18", style: new TextStyle(color: new Color(0xff00e5ff))),
+                                              new Text("2017", style: new TextStyle(color: new Color(0xff00e5ff)))
+                                            ],
+                                          ),
+                                          trailing: new Image.network("http://i67.tinypic.com/1zclu7t.jpg",
+                                            height: 50.0,
+                                            width: 50.0,
+                                          ),
+                                        ),
+                                        new EventsListTile(
+                                          leadingColor: const Color(0xff0072b9),
+                                          leading: new SizedBox(
+                                            height: 75.0,
+                                            width: 140.0,
+                                            child: new Image.network("http://i64.tinypic.com/561px.png"),
+                                          ),
+                                          title: new Text("Commonwealth Classic", style: new TextStyle(color: Colors.white)),
+                                          subtitle: new Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              new Text("November 3 - 5", style: new TextStyle(color: new Color(0xff00e5ff))),
+                                              new Text("2017", style: new TextStyle(color: new Color(0xff00e5ff)))
+                                            ],
+                                          ),
+                                        ),
+                                        new EventsListTile(
+                                          leadingColor: const Color(0xff3e2054),
+                                          leading: new SizedBox(
+                                            height: 75.0,
+                                            width: 140.0,
+                                            child: new Image.network("http://i65.tinypic.com/wrhw9h.png"),
+                                          ),
+                                          title: new Text("Golden Star Dance Championship", style: new TextStyle(color: Colors.white)),
+                                          subtitle: new Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              new Text("January 27 - 29", style: new TextStyle(color: new Color(0xff00e5ff))),
+                                              new Text("2017", style: new TextStyle(color: new Color(0xff00e5ff)))
+                                            ],
+                                          ),
+                                          trailing: new Image.network("http://i67.tinypic.com/1zclu7t.jpg",
+                                            height: 50.0,
+                                            width: 50.0,
+                                          ),
+                                        ),
+                                        new EventsListTile(
+                                          leading: new SizedBox(
+                                            height: 75.0,
+                                            width: 140.0,
+                                            child: new Image.network("http://i67.tinypic.com/15pk9ec.png"),
+                                          ),
+                                          title: new Text("Paragon Open Dancesport Championship", style: new TextStyle(color: Colors.white, fontSize: 15.5)),
+                                          subtitle: new Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              new Text("October 26 - 29", style: new TextStyle(color: new Color(0xff00e5ff))),
+                                              new Text("2017", style: new TextStyle(color: new Color(0xff00e5ff)))
+                                            ],
+                                          ),
+                                        ),
+                                        new EventsListTile(
+                                          leadingColor: Colors.black,
+                                          leading: new SizedBox(
+                                            height: 75.0,
+                                            width: 140.0,
+                                            child: new Image.network("http://i65.tinypic.com/a41imq.png"),
+                                          ),
+                                          title: new Text("Manhattan Dancesport Championship", style: new TextStyle(color: Colors.white)),
+                                          subtitle: new Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              new Text("Jun 23 - Jul 3", style: new TextStyle(color: new Color(0xff00e5ff))),
+                                              new Text("2017", style: new TextStyle(color: new Color(0xff00e5ff)))
+                                            ],
+                                          ),
+                                        ),
+                                      ]
+                                  )
+                              ),
+                            ],
+                          )
+                      )
+                    ],
+                  )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/src/widget/EventsTile.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+/*
+  [EventsListTile] extends class [ListTile]
+  The class is tailored for the Application's Events menu
+ */
+class EventsListTile extends ListTile {
+  Color leadingColor;
+  EventsListTile({this.leadingColor, Widget leading, Widget trailing, Widget title, Widget subtitle, GestureTapCallback onTap})
+      : super(leading: leading, trailing: trailing, title: title, subtitle: subtitle, onTap: onTap);
+
+  bool _denseLayout(ListTileTheme tileTheme) {
+    return dense != null ? dense : (tileTheme?.dense ?? false);
+  }
+
+  Color _iconColor(ThemeData theme, ListTileTheme tileTheme) {
+    if (!enabled)
+      return theme.disabledColor;
+
+    if (selected && tileTheme?.selectedColor != null)
+      return tileTheme.selectedColor;
+
+    if (!selected && tileTheme?.iconColor != null)
+      return tileTheme.iconColor;
+
+    switch (theme.brightness) {
+      case Brightness.light:
+        return selected ? theme.primaryColor : Colors.black45;
+      case Brightness.dark:
+        return selected ? theme.accentColor : null; // null - use current icon theme color
+    }
+    assert(theme.brightness != null);
+    return null;
+  }
+
+  Color _textColor(ThemeData theme, ListTileTheme tileTheme, Color defaultColor) {
+    if (!enabled)
+      return theme.disabledColor;
+
+    if (selected && tileTheme?.selectedColor != null)
+      return tileTheme.selectedColor;
+
+    if (!selected && tileTheme?.textColor != null)
+      return tileTheme.textColor;
+
+    if (selected) {
+      switch (theme.brightness) {
+        case Brightness.light:
+          return theme.primaryColor;
+        case Brightness.dark:
+          return theme.accentColor;
+      }
+    }
+    return defaultColor;
+  }
+
+  TextStyle _subtitleTextStyle(ThemeData theme, ListTileTheme tileTheme) {
+    final TextStyle style = theme.textTheme.body1;
+    final Color color = _textColor(theme, tileTheme, theme.textTheme.caption.color);
+    return _denseLayout(tileTheme)
+        ? style.copyWith(color: color, fontSize: 12.0)
+        : style.copyWith(color: color);
+  }
+
+  TextStyle _titleTextStyle(ThemeData theme, ListTileTheme tileTheme) {
+    final TextStyle style = tileTheme?.style == ListTileStyle.drawer
+        ? theme.textTheme.body2
+        : theme.textTheme.subhead;
+    final Color color = _textColor(theme, tileTheme, style.color);
+    return _denseLayout(tileTheme)
+        ? style.copyWith(fontSize: 13.0, color: color)
+        : style.copyWith(color: color);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterial(context));
+    final ThemeData theme = Theme.of(context);
+    final ListTileTheme tileTheme = ListTileTheme.of(context);
+
+    final bool isTwoLine = !isThreeLine && subtitle != null;
+    final bool isOneLine = !isThreeLine && !isTwoLine;
+    double tileHeight;
+    if (isOneLine)
+      tileHeight = _denseLayout(tileTheme) ? 48.0 : 56.0;
+    else if (isTwoLine)
+      tileHeight = _denseLayout(tileTheme) ? 60.0 : 72.0;
+    else
+      tileHeight = _denseLayout(tileTheme) ? 76.0 : 88.0;
+    // Overall, the list tile is a Row() with these children.
+    final List<Widget> children = <Widget>[];
+
+    IconThemeData iconThemeData;
+    if (leading != null || trailing != null)
+      iconThemeData = new IconThemeData(color: _iconColor(theme, tileTheme));
+
+    if (leading != null) {
+      children.add(IconTheme.merge(
+        data: iconThemeData,
+        child: new Container(
+          margin: const EdgeInsetsDirectional.only(end: 10.0),
+          padding: const EdgeInsets.all(5.0),
+          width: 140.0,
+          color: leadingColor != null ? leadingColor : new Color(0xffffffff),
+          alignment: AlignmentDirectional.centerStart,
+          child: leading,
+        ),
+      ));
+    }
+
+    final Widget primaryLine = new AnimatedDefaultTextStyle(
+        style: _titleTextStyle(theme, tileTheme),
+        duration: kThemeChangeDuration,
+        child: title ?? new Container()
+    );
+    Widget center = primaryLine;
+    if (subtitle != null && (isTwoLine || isThreeLine)) {
+      center = new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          primaryLine,
+          new AnimatedDefaultTextStyle(
+            style: _subtitleTextStyle(theme, tileTheme),
+            duration: kThemeChangeDuration,
+            child: subtitle,
+          ),
+        ],
+      );
+    }
+    children.add(new Expanded(
+      child: center,
+    ));
+
+    if (trailing != null) {
+      children.add(IconTheme.merge(
+        data: iconThemeData,
+        child: new Container(
+          //color: Colors.amber,
+          margin: const EdgeInsetsDirectional.only(start: 3.0, end: 5.0),
+          alignment: AlignmentDirectional.centerEnd,
+          child: trailing,
+        ),
+      ));
+    }
+
+    return new InkWell(
+        onTap: enabled ? onTap : null,
+        onLongPress: enabled ? onLongPress : null,
+        child: new Container(
+          decoration: new BoxDecoration(
+            border: new Border(
+              bottom: new BorderSide(
+                  color: Colors.black
+              ),
+            ),
+          ),
+          height: 80.0,
+          //padding: const EdgeInsets.only(top: 1.5, bottom: 1.5),
+          child: new Row(children: children),
+        )
+    );
+  }
+}
