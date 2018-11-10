31a503d1fe69895d65fa26b88a66beb7e14d47e2
Art Astillero <nanix84@yahoo.com>
2018-11-10 10:28:46 +0800

    added submit timestamp

--- lib/src/dao/FeedbackDao.dart
+++ lib/src/dao/FeedbackDao.dart
@@ -3,23 +3,27 @@ import 'dart:convert' show json;
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:myapp/MFGlobals.dart' as global;
 
 final reference = FirebaseDatabase.instance.reference().child("feedbacks");
 
 class FeedbackDao {
   static Future saveFeedback(String msg) async {
+    final formatterSrc = new DateFormat("yyyy-MM-dd");
     FirebaseUser fuser = await FirebaseAuth.instance.currentUser();
 
     String data = await rootBundle.loadString('mainframe_assets/conf/config.json');
     var result = json.decode(data);
     String app_version = result['app_version'];
+    DateTime dt = new DateTime.now().toUtc();
 
     return reference.push().set({
       "id":fuser.uid,
       "email": fuser.email,
       "app_version" : app_version,
       "device": global.devicePlatform,
+      "dateSubmitted": formatterSrc.format(dt),
       "message": msg
     });
   }
